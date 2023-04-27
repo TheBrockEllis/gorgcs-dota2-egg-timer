5938ef9291d5d831ed672f3474ec9790ac1d3eed
Brock Ellis <brock@sharproot.com>
2023-04-27 14:33:00 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: "Gorgc's Dota2 Egg Timer",
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
